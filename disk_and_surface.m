33c876390dd7f5e701176402c0169b72d09ad8dc
arturofburgos <burgos.arturo.ufu@gmail.com>
2020-07-16 22:23:39 -0300

    Add changes

--- disk_and_surface.m
+++ disk_and_surface.m
@@ -1,89 +1,95 @@
+% Undergraduate Student: Arturo Burgos
+% Professor: Aristeu da Silveira Neto
+% Federal University of Uberlândia - UFU, Fluid Mechanics Laboratory - MFLab, Block 5P, Uberlândia, MG, Brazil
+
+
 close all
 clear all
 clc
 
-% Disco receptor e oscilador
+% Disk and flat surface
 
-% Dados Fixos
+% Variables
 
-Patm = 101250; % pressão atmosférica [Pa]
+Patm = 101250; % Atmospheric pressure [Pa]
+mp = 9; % Disk mass [kg]
+g = 9.81; % Gravity [m/s^2]
+dp = 0.02; % Disk diameter [m]
+Ap = pi * ( dp/2 )^2; % Disk area [m^2]
+mu = 17.72e-6; % Dynamic Viscosity [N*s/m^2]
 
-mp = 9; % massa pistão [kg]
+Cp = 1004.8; % Specific heat [J/kg*K]
+R = 287; % Gas constant [J/kg*K]
+mu_oleo = 2.9e-1; % Oil viscosity [N*s/m^2]
+e = 0.8e-6; % Oil gap [m]
+hp = 2e-3; % Disk thickness [m]
+smax = 0.00092; % Maximum amplitude of the surface [m]
+f = 9; % Vibration frequency of the surface [Hz]
 
-g = 9.81; % aceleração da gravidade [m/s^2]
 
-dp = 0.02; % diametro do pistão [m]
 
-Ap = pi * ( dp/2 )^2; % area do pistão [m^2]
 
-mu = 17.72e-6; % viscosidade dinâmica [N*s/m^2]
+% Initial Conditions
 
-Cp = 1004.8; % calor especifico [J/kg*K]
+L = 0.01; % Initial lenght of the disk [m]
+sb = 0; % Initial lenght of the surface [m] 
+rho = 1.22; % Initial specific mass [kg/m^3]
 
-R = 287; % Constante do gás [J/kg*K]
+t = 0; % Initial time [s]
+tf = 3; % Final time [s]
+dt = 0.0000004; % Time step [s], in the beginning it was 0.00000005
+np = (tf-t)/dt; % Number of steps
 
-mu_oleo = 2.9e-1; % Viscosidade do Oleo [N*s/m^2]
+T = 15 + 273; % Initial Temperature [K]
+V = 0; % Initial disk speed [m/s]
+Vb = 0; % Initial surface speed [m/s]  
 
-e = 0.8e-6; % Gap de Oleo [m]
+mg = rho*Ap*L; % Mass of gas contained in the container [kg] (SHOULD BE CLOSE TO CONSTANT)
 
-hp = 2e-3; % Espessura do pistão [m]
 
-smax = 0.00092;
 
-% Dados Iniciais
 
-L = 0.01; % Comprimento inicial receptor [m]
+% Creation of the initial and final dy/dt arrays
 
-sb = 0; % Comprimento inicial oscilador [m] 
+yn_1 = [ V rho L T mg sb Vb]; % Initial array
+yn = [ 0 0 0 0 0 0 0 ]; % Final array
 
-rho = 1.22; % massa específica inicial [kg/m^3]
 
-t = 0; % tempo inicial [s]
-tf = 3; % tempo final [s]
-dt = 0.0000004; % passo de tempo[s] inicialmente 0.00000005
 
-np = (tf-t)/dt; % numero de passos
 
-T = 15 + 273; % Temperatura inicial [K]
-V = 0; %Velociade inicial receptor [m/s]
-Vb = 0; %Velocidade inicial do excitador [m/s]  
+% Criation of the matrix m 
 
-mg = rho*Ap*L; % massa de gás contida no recipiente [kg] (DEVERÁ APRESENTAR UM COMPORTAMENTO MAIS OU MENOS FIXO)
+m = zeros(8, np); 
 
-% Criação dos vetores dy/dt final e inicial
 
-yn_1 = [ V rho L T mg sb Vb]; % vetor inicial
-yn = [ 0 0 0 0 0 0 0 ]; % vetor final 
-
-% Criação da matriz que em cada divisão armazena os valores das iterações
-
-m = zeros(8, np); % matriz m de 4 linhas e np colunas
-
-f= 9;
 for i = 1:np
     
-    m(1,i) = t; % a matriz recebe na linha 1 os tempos nos intervalos np
-    m(2,i) = yn_1(1); % a matriz recebe na linha 2 as velocidades do receptor nos intervalos np
-    m(3,i) = yn_1(2); % a matriz recebe na linha 3 os rho nos intervalos np 
-    m(4,i) = yn_1(3); % a matriz recebe na linha 4 os L nos intervalos np
-    m(5,i) = yn_1(4); % a matriz recebe na linha 5 os T nos intervalos np
-    m(6,i) = yn_1(5); % a matriz recebe na linha 6 os mg nos intervalos np
-    m(7,i) = yn_1(6); % a matriz recebe na linha 7 os sb nos intervalos np
-    m(8,i) = yn_1(7); % a matriz recebe na linha 8 os Vb nos intervalos np
+    m(1,i) = t; % the matrix m gets in the row 1 the time
+    m(2,i) = yn_1(1); % a matrix m gets in the row 2 the speed of the disk 
+    m(3,i) = yn_1(2); % a matrix m gets in the row 3 the rho  
+    m(4,i) = yn_1(3); % a matrix m gets in the row 4 the L 
+    m(5,i) = yn_1(4); % a matrix m gets in the row 5 the T 
+    m(6,i) = yn_1(5); % a matrix m gets in the row 6 the mg 
+    m(7,i) = yn_1(6); % a matrix m gets in the row 7 the sb 
+    m(8,i) = yn_1(7); % a matrix m gets in the row 8 the Vb 
     
     % yn_1(1) = V
     % yn_1(2) = rho
     % yn_1(3) = L
     % yn_1(4) = T
+    % yn_1(5) = mg
+    % yn_1(6) = sb
+    % yn_1(7) = Vb
+    
         
-        yn(1) = yn_1(1) + dt * ( ( (  ( yn_1(2) * R * yn_1(4) - Patm ) * Ap ) / mp ) - g - ( mu_oleo * yn_1(1) * pi * dp * hp / (e*mp) ) );
-        yn(2) = yn_1(2) - ( dt * ( yn_1(2) * ( yn(1) - yn_1(7) ) ) ) / yn_1(3);
-        yn(3) = yn_1(3) - dt * ( yn_1(7) - yn(1) );
-        yn(4) = yn_1(4) +  dt * ( (4/3 * mu * ( ( ( yn(1) - yn_1(7) ) / yn(3) )^2 ) / ( yn(2) * Cp ) ) + ( ( (yn(2) * R *yn_1(4)-Patm) * ( yn(2) - yn_1(2) ) ) / (( yn(2) * Cp ) * ( yn(2) *dt )) ) ) ;
+        yn(1) = yn_1(1) + dt * ((((yn_1(2) * R * yn_1(4) - Patm) * Ap) / mp) - g - (mu_oleo * yn_1(1) * pi * dp * hp / (e*mp)));
+        yn(2) = yn_1(2) - (dt * (yn_1(2) * (yn(1) - yn_1(7)))) / yn_1(3);
+        yn(3) = yn_1(3) - dt * (yn_1(7) - yn(1));
+        yn(4) = yn_1(4) +  dt * ((4/3 * mu * (((yn(1) - yn_1(7)) / yn(3))^2) / (yn(2) * Cp)) + (((yn(2) * R *yn_1(4)-Patm) * (yn(2) - yn_1(2))) / (( yn(2) * Cp) * (yn(2) *dt))));
         %yn(4) = yn_1(4);
         yn(5) = yn(2)*yn(3)*Ap;
-        yn(6) = smax * sin( 2 * pi * f * (t + pi/2));
-        yn(7) = smax * 2 * pi * f * cos( 2 * pi * f * t);
+        yn(6) = smax * sin(2 * pi * f * (t + pi/2));
+        yn(7) = smax * 2 * pi * f * cos(2 * pi * f * t);
         
         
     t = t + dt;
@@ -93,30 +99,31 @@ end
 
 figure
 plot(m(1,:),m(2,:))
-title(' Velocidade do Disco');
-xlabel('Tempo [s]');
-ylabel('Velocidade [m/s]');
+title('Disk Speed');
+xlabel('Time [s]');
+ylabel('Speed [m/s]');
 grid minor
+
 
 figure
 plot(m(1,:),m(3,:))
-title('Variação da Massa específica do Disco');
-xlabel('Tempo [s]');
+title('Disk Specific Mass Variation');
+xlabel('Time [s]');
 ylabel('\rho [kg/m^3]');
 grid minor
 
 
 figure
 plot(m(1,:),m(4,:),m(1,:),m(7,:))
-title('Variação da Altura');
-xlabel('Tempo [s]');
+title('Height Variation');
+xlabel('Time [s]');
 ylabel('H [m]');
 grid minor
 
 
 figure
 plot(m(1,:),m(5,:))
-title('Variação da Temperatura');
-xlabel('Tempo [s]');
-ylabel('Temperatura [K]');
+title('Temperature Variation');
+xlabel('Time [s]');
+ylabel('Temperature [K]');
 grid minor
